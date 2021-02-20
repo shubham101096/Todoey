e5054b7e69a0b8ebb00797197e9644e4577eadd5
Shubham Mishra <shubhammishra10@gmail.com>
2021-02-20 16:52:51 +0530

    navigation bar added

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = [1, 2, 3, 4, 5]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+
+}
+
